f80c92ddee03a92ca4fc3a13578a1e4ae694042f
LucasMartines01 <lucas.martines@izio.com.br>
2024-05-02 21:40:04 -0300

    feat: change api instance to set authentication token

--- app/src/main/java/com/example/ezliv_mobile/ui/provider/Api.kt
+++ app/src/main/java/com/example/ezliv_mobile/ui/provider/Api.kt
@@ -1,15 +1,38 @@
 package com.example.ezliv_mobile.ui.provider
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
     const val BASE_URL = "https://61c71fed90318500175472ff.mockapi.io/api/"
 
+    private var authToken: String? = null
+
+    fun setAuthToken(token: String) {
+        authToken = token
+    }
+
     fun getInstance() : Retrofit {
+        val httpClient = OkHttpClient.Builder()
+
+        val interceptor = Interceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+            authToken?.let {
+                requestBuilder.header("Authorization", "Bearer $it")
+            }
+            val request = requestBuilder.method(original.method, original.body).build()
+            chain.proceed(request)
+        }
+
+        httpClient.addInterceptor(interceptor)
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
             .build()
     }
 }
